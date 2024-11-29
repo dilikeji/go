d9d981e0d6c24b42a70fb227d6115a00319b846e
tkskzhoudi <1683481717@qq.com>
2024-11-29 16:11:22 +0800

    init

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func GlobalMiddleware(gReq *ghttp.Request) {
 	)
 	// 执行业务逻辑前
 	g.Log().Info(gReq.Context(), "Before Request")
-	// 获取请求头 鉴权
+	// 获取请求头 鉴权 配置中未设置authHeader 或没有对应请求头则跳过 鉴权移到业务代码中获取上下文时
 	if header, err = g.Cfg().Get(gReq.Context(), "server.authHeader"); err == nil {
 		auth := gReq.Header.Get(header.String())
 		// 无视类型输出变量到控制台
@@ -36,21 +36,25 @@ func GlobalMiddleware(gReq *ghttp.Request) {
 		if !g.IsEmpty(auth) {
 			// redis获取变量
 			if redisValue, err = g.Redis().Get(gReq.Context(), auth); err != nil {
+				// redis连接失败报错
 				gReq.Response.WriteJsonExit(g.Map{
 					"code": App.CodeToken,
-					"msg":  "",
+					"msg":  err.Error(),
 					"data": make([]interface{}, 0),
 				})
 				return
 			}
-			gReq.SetCtxVar("Auth", redisValue)
-			redisValue = g.NewVar(g.Map{
-				"timeNow": gtime.Now().String(),
-			})
-			// redis设置变量 有效期秒
-			err = g.Redis().SetEX(gReq.Context(), auth, redisValue, 1000)
+			if !redisValue.IsEmpty() {
+				gReq.SetCtxVar("Auth", redisValue)
+				redisValue = g.NewVar(g.Map{
+					"timeNow": gtime.Now().String(),
+				})
+				// redis设置变量 有效期秒
+				err = g.Redis().SetEX(gReq.Context(), auth, redisValue, 1000)
+			}
 		}
 	}
+	// TODO 验签
 	gReq.Middleware.Next()
 	// 执行业务逻辑后
 	g.Log().Info(gReq.Context(), "After Request")
